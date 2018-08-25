5e68455c723c65854fbdce0db5f33b21a67ec09c
Saswatm123 <33075084+Saswatm123@users.noreply.github.com>
2018-08-24 22:15:36 -0700

    Overwrite undo
    Github keeps overwriting the text of the actual code for some reason so this is what the last commit should've looked like. This is the backup version of the last commit without the random insertions github puts into my code files.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define BITS_Q_STATES 9u //Inp bits required for storing max amount of variables
 
 #include <iostream>
@@ -21,27 +20,3 @@ int main()
     PERM unknown acceleration;
     PERM unknown stuff;
 }
-=======
-#define BITS_Q_STATES 9u //Inp bits required for storing max amount of variables
-
-#include <iostream>
-#include <typeinfo>
-#include "var.hpp"
-
-#ifndef PERM
-    #define PERM static constexpr const
-#endif // PERM
-
-#define known   var<ticket(),1>
-#define unknown var<ticket(),0>
-
-using namespace std;
-
-int main()
-{
-    PERM known force(12);
-    PERM known mass(3);
-    PERM unknown acceleration;
-    PERM unknown stuff;
-}
->>>>>>> 6b4acc599ff09c04d9bbcac5b5a808dc19dc6208
